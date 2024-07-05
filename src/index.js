0ee84c353edea835d8610560da900c3f9683db80
cfworker <mfs860908@gmail.com>
2024-07-05 09:45:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.cfworker.github.io": dockerHub,
+  "quay.cfworker.github.io": "https://quay.io",
+  "gcr.cfworker.github.io": "https://gcr.io",
+  "k8s-gcr.cfworker.github.io": "https://k8s.gcr.io",
+  "k8s.cfworker.github.io": "https://registry.k8s.io",
+  "ghcr.cfworker.github.io": "https://ghcr.io",
+  "cloudsmith.cfworker.github.io": "https://docker.cloudsmith.io",
+  "ecr.cfworker.github.io": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
